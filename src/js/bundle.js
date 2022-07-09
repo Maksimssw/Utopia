b5a5af68bb39fb9c147220335454b08c1602cfd1
Maks <kiva_hill@mail.ru>
2022-07-09 14:15:28 +0300

    first commit

---
+++ src/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+document.addEventListener('click', function(){
+    
+});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
